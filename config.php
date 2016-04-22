48b74bd42b1db6d4b78b899037817b33310e6eb1
dufay-theo <dufay.theo@gmail.com>
2016-04-22 12:04:16 +0200

    [Init] push online

--- config.php
+++ config.php
@@ -1,19 +1,21 @@
 <?php
+
 switch ($_SERVER['SERVER_NAME']) {
     case 'www.simplepost.xyz' : case 'simplepost.xyz' :
-        define('BASE_URL','http://www.simplepost.xyz//single-post/');
-        define('HOST','localhost');
-        define('DBNAME','single-post');
-        define('USERNAME','root');
-        define('PASSWORD','root');
+	define('BASE_URL', 'http://www.simplepost.xyz//single-post/');
+	define('HOST', 'localhost');
+	define('DBNAME', 'single-post');
+	define('USERNAME', 'root');
+	define('PASSWORD', 'root');
+	break;
     default :
-        define('BASE_URL','http://127.0.0.1/single-post/');
-        define('HOST','localhost');
-        define('DBNAME','single-post');
-        define('USERNAME','root');
-        define('PASSWORD','root');
+	define('BASE_URL', 'http://127.0.0.1/single-post/');
+	define('HOST', 'localhost');
+	define('DBNAME', 'single-post');
+	define('USERNAME', 'root');
+	define('PASSWORD', 'root');
 }
 
-ORM::configure('mysql:host='.HOST.';dbname='.DBNAME);
+ORM::configure('mysql:host=' . HOST . ';dbname=' . DBNAME);
 ORM::configure('username', USERNAME);
 ORM::configure('password', PASSWORD);
